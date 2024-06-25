ba7d01d35e444777e3d5f678abe09795254e62f7
martus1974 <cmcastillo79@gmail.com>
2024-06-25 17:13:52 -0300

    juego num secreto

---
+++ app.js
@@ -0,0 +1,69 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+function verificarIntento() {
+  let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}`)
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else {
+    //el usuario no acerto.
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento('p','el numero secreto es menor');
+    } else {
+    asignarTextoElemento('p','el numero secreto es mayor');  
+    }
+    intentos++;
+    limpiarCaja();
+  }
+  return;
+}
+
+function limpiarCaja() {
+  document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+  let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+  
+  console.log(numeroGenerado);
+  console.log(listaNumerosSorteados);
+   //si ya sorteamos todos los numeros
+
+  if (listaNumerosSorteados.length === numeroMaximo) {
+     asignarTextoElemento('p','ya se sortearon todos los numeros posibles');
+  } else {
+   // si el numero generado ya esta en la lista
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+      return generarNumeroSecreto();
+  } else {
+    listaNumerosSorteados.push(numeroGenerado);
+      return numeroGenerado;
+    }
+  }
+}
+function condicionesIniciales(){
+  asignarTextoElemento('h1','Juego del numero secreto');
+  asignarTextoElemento('p',`Indica un numero del 1 al ${numeroMaximo}`);
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+  document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+
+function reiniciarJuego() {
+  limpiarCaja();
+  condicionesIniciales();
+  
+}
+condicionesIniciales();
